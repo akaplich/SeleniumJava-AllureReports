91a2c10952d3fca62ebe0b0961b72c0895c15ad1
Adem Cooper <acooper@brightidea.com>
2025-04-14 13:20:35 -0700

    Added browser to testBase so we don't have to instantiate it for every testcase

--- src/test/java/BaseClasses/TestBase.groovy
+++ src/test/java/BaseClasses/TestBase.groovy
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeSuite
 import org.testng.annotations.AfterMethod
 
 public class TestBase {
+    public static Browser browser = Browser.getInstance()
 
     @BeforeSuite
     public void beforeState(){
