e694dab79a257787d9e70d8086b60d16ae9125de
Adem Cooper <acooper@brightidea.com>
2025-04-18 16:12:55 -0700

    More chrome options

--- src/test/groovy/TestCases/FirstGroovyScriptTest.groovy
+++ src/test/groovy/TestCases/FirstGroovyScriptTest.groovy
@@ -15,12 +15,31 @@ public class FirstGroovyScriptTest {
         //System.setProperty("webdriver.chrome.driver", "/Users/Brightidea/Documents/chromedriver-mac-arm64/chromedriver");
         WebDriverManager.chromedriver().setup();
         // Create a unique temporary directory for user data
-        Path tempDir = Files.createTempDirectory("chrome-user-data");
+        //Path tempDir = Files.createTempDirectory("chrome-user-data");
 
         // Set Chrome options
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
-
+        //options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
+        //options.addArguments("--no-first-run", "--no-default-browser-check");
+        options.addArguments(
+                "--no-sandbox",
+                "--no-first-run",
+                "--display=:10",
+                "--window-size=2560,1440",
+                "--enable-chrome-browser-cloud-management",
+                "--remote-debugging-port=9222",
+                "--start-maximized",
+                "--enable-automation",
+                "--xkb",
+                "--noxrecord",
+                "--noxfixes",
+                "--noxdamage",
+                "--use-gl=swiftshader",
+                "--enable-logging",
+                "--log-file=/var/log/rwhq/chromium-debug.log",
+                "--skip-force-online-signin-for-testing",
+                "--disable-popup-blocking"
+        )
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         System.out.println(driver.getTitle());
