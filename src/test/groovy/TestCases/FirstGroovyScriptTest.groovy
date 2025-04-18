9d6ddec46b82137e253e57ae0b8e5cc9af9044c8
Adem Cooper <acooper@brightidea.com>
2025-04-18 16:44:28 -0700

    Logger cleanup and chrome installation check added

--- src/test/groovy/TestCases/FirstGroovyScriptTest.groovy
+++ src/test/groovy/TestCases/FirstGroovyScriptTest.groovy
@@ -5,19 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import java.nio.file.Files
 import java.nio.file.Path;
 
 public class FirstGroovyScriptTest {
+    private static final Logger logger = LoggerFactory.getLogger(FirstGroovyScriptTest.class);
+
     @Test
     public void main() {
         //System.setProperty("webdriver.chrome.driver", "/Users/Brightidea/Documents/chromedriver-mac-arm64/chromedriver");
+        logger.debug("Downloading ChromeDriver");
         WebDriverManager.chromedriver().setup();
         // Create a unique temporary directory for user data
         //Path tempDir = Files.createTempDirectory("chrome-user-data");
 
         // Set Chrome options
+        logger.debug("Setting Options");
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
         //options.addArguments("--no-first-run", "--no-default-browser-check");
@@ -40,9 +46,15 @@ public class FirstGroovyScriptTest {
                 "--skip-force-online-signin-for-testing",
                 "--disable-popup-blocking"
         )
+
+        logger.debug("Assigning Chrome Driver");
         WebDriver driver = new ChromeDriver();
+
+        logger.debug("Navigate to URL");
         driver.get("http://www.google.com");
-        System.out.println(driver.getTitle());
+
+        logger.debug("Url Title: " + driver.getTitle());
+        logger.debug("Quitting");
         driver.quit();
     }
 }
