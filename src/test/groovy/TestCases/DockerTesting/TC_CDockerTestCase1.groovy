96edcc87622b421b6af092d17d17472396a7c3bd
Adem Cooper <acooper@brightidea.com>
2025-04-22 08:30:11 -0700

    Updates to Browser so smoke cases can run on docker containers

--- src/test/groovy/TestCases/DockerTesting/TC_CDockerTestCase1.groovy
+++ src/test/groovy/TestCases/DockerTesting/TC_CDockerTestCase1.groovy
@@ -10,14 +10,14 @@ import org.openqa.selenium.remote.RemoteWebDriver
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-public class TC_CSelenoidDockerTestCase1 {
+public class TC_CDockerTestCase1 {
     private static final Logger logger = LoggerFactory.getLogger(FirstGroovyScriptTest.class);
+    public static WebDriver Driver = null
 
     @Test
-    @Tag("SelenoidSet")
+    @Tag("DockerSet")
     public void main() {
         //System.setProperty("webdriver.chrome.driver", "/Users/Brightidea/Documents/chromedriver-mac-arm64/chromedriver");
-        logger.debug("Downloading ChromeDriver");
         WebDriverManager.chromedriver().setup();
         // Create a unique temporary directory for user data
         //Path tempDir = Files.createTempDirectory("chrome-user-data");
@@ -25,16 +25,18 @@ public class TC_CSelenoidDockerTestCase1 {
         Map<String, Object> options = new HashMap<>()
         options.put("enableVNC", true)
         options.put("enableVideo", false)
-
+        options.put("ignore-ssl-errors", true)
+        options.put("ignore-certificate-errors", false)
+        
         ChromeOptions chromeOptions = new ChromeOptions()
         chromeOptions.setCapability("se:options", options)
 
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions)
+        Driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions)
 
         logger.debug("Navigate to URL");
-        driver.get("http://www.google.com");
+        Driver.get("http://www.google.com");
 
-        logger.debug("Url Title: " + driver.getTitle());
+        logger.debug("Url Title: " + Driver.getTitle());
         Thread.sleep(1000)
         assert 2 + 2 == 4 : "Math error: 2 + 2 should equal 4"
 
@@ -42,6 +44,6 @@ public class TC_CSelenoidDockerTestCase1 {
     @AfterEach
     public void afterState(){
         logger.debug("Quitting");
-        driver.quit();
+        Driver.quit();
     }
 }
