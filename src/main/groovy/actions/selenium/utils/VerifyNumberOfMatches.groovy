713ed95b8aa5c53acc2078c61f182f6674b1727f
Shefali Raje <sraje@brightidea.com>
2025-04-30 23:59:31 -0700

    Update Pipeline Rules Engine test cases Part1

--- src/main/groovy/actions/selenium/utils/VerifyNumberOfMatches.groovy
+++ src/main/groovy/actions/selenium/utils/VerifyNumberOfMatches.groovy
@@ -1,6 +1,15 @@
 package actions.selenium.utils;
 
 import actions.selenium.Exists
+import actions.selenium.Browser
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait
+import org.openqa.selenium.WebElement
+import actions.selenium.utils.Elements
+import java.time.Duration
 
 class VerifyNumberOfMatches{
     public static void run(def params){
@@ -13,17 +22,45 @@ class VerifyNumberOfMatches{
         if(params.Timeout==null){params.Timeout=30}
         if(params."Is Displayed"){params."Is Displayed"=true}
         else{params."Is Displayed"=false}
-        def instancesFound = Exists.run(params)
-        println("InstancesFound: ${instancesFound}")
-        //If number matches is not null, this means we expect specific number of matches found
-        if(params."Number of Matches"!=null){
-            assert instancesFound==params."Number of Matches","Error - Expected number of matches ${params."Number of Matches"} not found, actual amount found is ${instancesFound}"
+        def parentElements = []
+        def wait = new WebDriverWait(Browser.Driver, Duration.ofSeconds(params.Timeout))
+       try {
+            // Wait for at least one parent to exist
+            wait.until {
+                parentElements = Browser.Driver.findElements(By.xpath(params.ID))
+                return parentElements.size() > 0
+            }
+        } catch (Exception e) {
+            parentElements = []
         }
-        //else any number as long as it is higher than 0 will do
-        else{
-            assert instancesFound>0,"Error - Expected instance not found"
+
+        def validMatches = 0
+
+        parentElements.each { parent ->
+            boolean allChildrenMatch = true
+
+            if (params.IDChildren && params.IDChildren instanceof List) {
+                for (childXPath in params.IDChildren) {
+                    def children = parent.findElements(By.xpath(childXPath))
+                    if (children.isEmpty()) {
+                        allChildrenMatch = false
+                        break
+                    }
+                }
+            }
+
+            if (allChildrenMatch) {
+                validMatches++
+            }
         }
-        
-        
+
+        println("Valid Matches with all children: ${validMatches}")
+
+        if (params."Number of Matches" != null) {
+            assert validMatches == params."Number of Matches" : 
+                "Error - Expected number of matches ${params."Number of Matches"} not found, actual valid matches found: ${validMatches}"
+        } else {
+            assert validMatches > 0 : "Error - Expected instance not found"
+        }
     }
 }
