4a0e390d25283d2fb10897a30ae0ba2ddb7f54ab
akaplich <akaplich@brightidea.com>
2025-06-02 16:36:11 -0400

    Finalize Allure reporting setup

---
+++ src/test/groovy/BaseClasses/FailureCaptureExtension.groovy
@@ -0,0 +1,70 @@
+package BaseClasses
+
+import io.qameta.allure.Allure
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.TestWatcher
+import actions.selenium.Browser
+import ru.yandex.qatools.ashot.AShot
+
+import javax.imageio.ImageIO
+
+import java.awt.image.BufferedImage
+
+
+class FailureCaptureExtension implements TestWatcher{
+    static final String RERUN_FILE_PATH = "target/surefire-reports/rerunFailingTests.txt"
+    static Set<String> recordedFailures = Collections.synchronizedSet(new HashSet<>()) //auto dedupe, thread safe
+
+    @Override
+    void testFailed(ExtensionContext context, Throwable cause){
+        try {
+            def outputFile = new File(RERUN_FILE_PATH)
+            def testClass = context.getRequiredTestClass().name
+            def testMethod = context.getRequiredTestMethod().name
+            def fullName = "$testClass#$testMethod"
+            String testName = context.getDisplayName()
+
+            // Save test name to rerun file
+            System.out.println(">>> ❌ Test failed: $fullName")
+
+            //prevent duplicates
+            if (recordedFailures.add(fullName)) {
+                // Only write if it wasn't already seen
+                println(">>> Saving test name to rerun file: $fullName")
+                outputFile.withWriterAppend { writer ->
+                    writer.writeLine(fullName)
+                }
+            }
+            // grab screenshot bytes
+            System.out.println(">>> 📸ScreenshotOnFailureExtension caught a failure in: " + testName)
+            BufferedImage fullScreenshot = new AShot()
+                    .takeScreenshot(Browser.Driver)
+                    .getImage();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(fullScreenshot, "PNG", baos);
+            byte[] bytes = baos.toByteArray();
+            Allure.getLifecycle().addAttachment(
+                    "Failure screenshot of $testName",
+                    "image/png",
+                    "png",
+                    new ByteArrayInputStream(bytes)
+            )
+        }catch (Exception e) {
+            // don’t let screenshot errors hide the original test failure
+            e.printStackTrace()
+        }finally {
+            if (Browser.Driver != null) {
+                System.out.println(">>> Quitting browser after failure screenshot.")
+                Browser.quitDriver()
+            }
+        }
+    }
+    void testSuccessful(ExtensionContext context) {
+        // Quit driver normally if it wasn't already
+        if (Browser.Driver != null) {
+            System.out.println(">>> Test passed. Quitting browser.")
+            Browser.quitDriver()
+        }
+    }
+}
--- src/test/groovy/BaseClasses/TestBase.groovy
+++ src/test/groovy/BaseClasses/TestBase.groovy
@@ -3,6 +3,9 @@ package BaseClasses
 import actions.API.Utils.DeleteAffiliate
 import actions.selenium.CloseWindow
 import actions.selenium.utils.OnFailureExtension
+import actions.selenium.Browser
+import io.qameta.allure.Attachment
+import io.qameta.allure.junit5.AllureJunit5
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
@@ -10,8 +13,11 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import io.qameta.allure.Step
 
 @ExtendWith(OnFailureExtension.class)
+@ExtendWith(FailureCaptureExtension)
+@ExtendWith(AllureJunit5.class)
 public class TestBase {
     private static final Logger logger = LoggerFactory.getLogger(TestBase.class);
 
@@ -35,7 +41,18 @@ public class TestBase {
     @AfterAll
     public static void tb_afterAll(){
         logger.debug("TestBase AfterAll");
-
+    }
+    //wrap each action in a step
+    //this will call the Allure wrapper that adds the Step annotation
+    protected def runWithStep(Object action, Map args, String stepName) {
+        stepWrapper(stepName) {
+            action.run(args)
+        }
+    }
+    //this is Allure wrapper that will call the action and add Step annotation to it
+    @Step("{stepName}")
+    protected def stepWrapper(String stepName, Closure closure) {
+        return closure.call()
     }
 
 }
