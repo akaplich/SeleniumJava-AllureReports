72201836914abdf2aa85e06e8608d774fd8e27db
Adem Cooper <acooper@brightidea.com>
2025-06-19 10:49:58 -0700

    Disabled C131043 and updated VerifyIdeainPipelineStepsViewpage() to match what is in RedwoodHQ

--- src/main/groovy/actions/PipelineStepsView/VerifyIdeainPipelineStepsViewpage.groovy
+++ src/main/groovy/actions/PipelineStepsView/VerifyIdeainPipelineStepsViewpage.groovy
@@ -7,22 +7,19 @@ class VerifyIdeainPipelineStepsViewpage{
     public void run(def params){
         //Map all values that will be used for verification for that instance
         def verify = [:]
-        def baseXPath = params."Placement Number"
-            ? "(//*[@id='content-inside'])[${params."Placement Number"}]/DIV//P[contains(.,'${params."Idea Name"}')]"
-            : "//*[@id='content-inside']/DIV//P[contains(.,'${params."Idea Name"}')]"
-        
-        // Add Idea ID constraint if provided
-        if (params."Idea ID") {
-            baseXPath += "[.//A[normalize-space()='${params."Idea ID"}']]"
+        if(params."Placement Number"){
+            verify.ID = "(//*[@id='content-inside'])[${params."Placement Number"}]/DIV//P[contains(.,'${params."Idea Name"}')]"
+        }
+        else{
+            verify.ID = "//*[@id='content-inside']/DIV//P[contains(.,'${params."Idea Name"}')]"
         }
 
-        // Add Step Name constraint if provided
-        if (params."Step Name idea belongs to") {
-            baseXPath += "[./../../../../../../../../../../../..//SPAN[@class='step_header_name' and text()='${params."Step Name idea belongs to"}']]"
-        }
-        verify.ID = baseXPath
+        verify.IDChildren = []
+        if(params."Idea ID"){verify.IDChildren << ".//A[normalize-space()='${params."Idea ID"}']"}
+        if(params."Step Name idea belongs to"){verify.IDChildren << "./../../../../../../../../../../../..//SPAN[@class='step_header_name' and text()='${params."Step Name idea belongs to"}']"}
         if(params."Number of Matches"){verify."Number of Matches" = params."Number of Matches".toInteger()}
         verify.Timeout=15
-        VerifyNumberOfMatches.run(verify) 
+        VerifyNumberOfMatches.run(verify)
     }
+
 }
--- src/test/groovy/TestCases/RWHQImports/OldRuleModal/C131043_OLDRULEMODALAddedtoStepwithsourceasWhiteboard_ChangeVisibility.groovy
+++ src/test/groovy/TestCases/RWHQImports/OldRuleModal/C131043_OLDRULEMODALAddedtoStepwithsourceasWhiteboard_ChangeVisibility.groovy
@@ -1,6 +1,7 @@
 package TestCases.RWHQImports.OldRuleModal
 
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import BaseClasses.TestBase
@@ -19,7 +20,8 @@ extends TestBase {
         variables."CodeEnvironment" = /Default/
         variables."Database" = null
     }
-    @Test @Tag("OldRulesModalRegression")  public void testcase(){
+    @Disabled @Test @Tag("OldRulesModalRegression")
+    public void testcase(){
         //Basestate
         Action58123c20fa4ee77809f468f6([:])
         //Navigate WebStorm or MTS
